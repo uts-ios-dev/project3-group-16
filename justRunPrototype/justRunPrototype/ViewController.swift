f03a2650e830c5b1d3ae9e9f5813f29a558421d9
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2018-05-14 10:18:32 +1000

    Added first prototype.
    The prototype is a single view app and is currently not using SpriteKit. The buttons work and take you to each view but the buttons and sliders on the Options view do not do anything.

---
+++ justRunPrototype/justRunPrototype/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  justRunPrototype
+//
+//  Created by Alexander Leydon on 14/5/18.
+//  Copyright © 2018 Alexander Leydon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func unwindToMenu(unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
